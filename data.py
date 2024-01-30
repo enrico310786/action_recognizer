3fd329982213b58ffd85e9691c222ec905dc7c53
Enrico <randellini@quest-it.com>
2024-01-30 15:13:34 +0100

    add scripts to train base model plus autoencoder

---
+++ data.py
@@ -0,0 +1,150 @@
+import torch
+import os
+
+from pytorchvideo.data.encoded_video import EncodedVideo
+from torch.utils.data.distributed import DistributedSampler
+from torch.utils.data import DataLoader
+
+from pytorchvideo.transforms import (
+    ApplyTransformToKey,
+    Normalize,
+    UniformTemporalSubsample
+)
+
+from torchvision.transforms import (
+    Compose,
+    Lambda,
+    Resize
+)
+
+
+class PackPathway(torch.nn.Module):
+    """
+    Transform for converting video frames as a list of tensors.
+    """
+    def __init__(self, alpha_slowfast):
+        super().__init__()
+        self.alpha_slowfast = alpha_slowfast
+
+    def forward(self, frames: torch.Tensor):
+        fast_pathway = frames
+        # Perform temporal sampling from the fast pathway.
+        slow_pathway = torch.index_select(
+            frames,
+            1,
+            torch.linspace(
+                0, frames.shape[1] - 1, frames.shape[1] // self.alpha_slowfast
+            ).long(),
+        )
+        frame_list = [slow_pathway, fast_pathway]
+        return frame_list
+
+
+class Dataset(torch.utils.data.Dataset):
+    def __init__(self, df_dataset, data_cfg, dataset_path, is_train=False, is_slowfast=False) -> None:
+        super().__init__()
+
+        self.df_dataset = df_dataset
+        self.dataset_path = dataset_path
+        self.data_cfg = data_cfg
+        self.num_frames_to_sample = self.data_cfg["num_frames_to_sample"]
+        self.mean = [float(i) for i in self.data_cfg["mean"]]
+        self.std = [float(i) for i in self.data_cfg["std"]]
+        self.resize_to = self.data_cfg["resize_to"]
+        self.permute_color_frame = self.data_cfg.get("permute_color_frame", 1.0) > 0.0
+        self.is_train = is_train
+        self.is_slowfast = is_slowfast
+        self.alpha_slowfast = data_cfg.get("alpha_slowfast", None)
+
+        # in this case i do not perform any runtime transformations. The data augmentation has been done offline
+        if not self.is_slowfast:
+            self.transform = ApplyTransformToKey(
+                key="video",
+                transform=Compose(
+                    [
+                        UniformTemporalSubsample(self.num_frames_to_sample),
+                        Lambda(lambda x: x / 255.0),
+                        Normalize(self.mean, self.std),
+                        Resize((self.resize_to, self.resize_to))
+                    ]
+                ),
+            )
+        else:
+            self.transform = ApplyTransformToKey(
+                key="video",
+                transform=Compose(
+                    [
+                        UniformTemporalSubsample(self.num_frames_to_sample),
+                        Lambda(lambda x: x / 255.0),
+                        Normalize(self.mean, self.std),
+                        Resize((self.resize_to, self.resize_to)),
+                        PackPathway(self.alpha_slowfast)
+                    ]
+                ),
+            )
+
+    def __len__(self):
+        return len(self.df_dataset)
+
+    def __getitem__(self, idx):
+        video_path = os.path.join(self.dataset_path, self.df_dataset.iloc[idx]["PATH_VIDEO"])
+        label = self.df_dataset.iloc[idx]["LABEL"]
+        classe = self.df_dataset.iloc[idx]["CLASS"]
+
+        video = EncodedVideo.from_path(video_path)
+        start_time = 0
+        # follow this post for clip duration https://towardsdatascience.com/using-pytorchvideo-for-efficient-video-understanding-24d3cd99bc3c
+        clip_duration = int(video.duration)
+        end_sec = start_time + clip_duration
+        video_data = video.get_clip(start_sec=start_time, end_sec=end_sec)
+        video_data = self.transform(video_data)
+        video_tensor = video_data["video"]
+        if self.permute_color_frame:
+            video_tensor = torch.permute(video_tensor, (1, 0, 2, 3))
+
+        return video_tensor, label, classe
+
+
+def create_loaders(df_dataset_train, df_dataset_val, df_dataset_anomaly, data_cfg, dataset_path, batch_size, is_slowfast=False):
+
+    # 1 - istanzio la classe dataset di train, val e test
+    classification_dataset_train = Dataset(df_dataset=df_dataset_train,
+                                           data_cfg=data_cfg,
+                                           dataset_path=dataset_path,
+                                           is_train=True,
+                                           is_slowfast=is_slowfast)
+    classification_dataset_val = Dataset(df_dataset=df_dataset_val,
+                                         data_cfg=data_cfg,
+                                         dataset_path=dataset_path,
+                                         is_slowfast=is_slowfast)
+
+    classification_dataset_anomaly = None
+    if df_dataset_anomaly is not None:
+        classification_dataset_anomaly = Dataset(df_dataset=df_dataset_anomaly,
+                                                 data_cfg=data_cfg,
+                                                 dataset_path=dataset_path,
+                                                 is_slowfast=is_slowfast)
+
+
+    # 2 - istanzio i dataloader
+    classification_dataloader_train = DataLoader(dataset=classification_dataset_train,
+                                                 batch_size=batch_size,
+                                                 shuffle=True,
+                                                 num_workers=0,
+                                                 drop_last=False)
+
+    classification_dataloader_val = DataLoader(dataset=classification_dataset_val,
+                                               batch_size=batch_size,
+                                               shuffle=True,
+                                               num_workers=0,
+                                               drop_last=False)
+
+    classification_dataloader_anomaly = None
+    if classification_dataset_anomaly is not None:
+        classification_dataloader_anomaly = DataLoader(dataset=classification_dataset_anomaly,
+                                                       batch_size=batch_size,
+                                                       shuffle=True,
+                                                       num_workers=0,
+                                                       drop_last=False)
+
+    return classification_dataloader_train, classification_dataloader_val, classification_dataloader_anomaly
--- model.py
+++ model.py
@@ -63,3 +63,130 @@ class R3D_slowfast(nn.Module):
     def forward(self, x):
         x = self.base_model(x)
         return x
+
+
+class Encoder(nn.Module):
+    def __init__(self,  init_dim, num_autoencoder_layers, dim_autoencoder_layers, dropout):
+        super(Encoder, self).__init__()
+        self.init_dim = init_dim
+        self.num_autoencoder_layers = num_autoencoder_layers
+        self.dim_autoencoder_layers = dim_autoencoder_layers
+        self.layer_1 = nn.Linear(self.init_dim, dim_autoencoder_layers[0])
+        self.layer_2 = nn.Linear(dim_autoencoder_layers[0], dim_autoencoder_layers[1])
+        self.layer_3 = None
+        if num_autoencoder_layers == 3:
+            self.layer_3 = nn.Linear(dim_autoencoder_layers[1], dim_autoencoder_layers[2])
+        self.dropout = nn.Dropout(dropout)
+        self.relu = nn.ReLU()
+        self.batchNorm1 = nn.BatchNorm1d(init_dim)
+
+    def forward(self, x):
+        x = self.batchNorm1(x)
+        x = self.layer_1(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+        x = self.layer_2(x)
+        x = self.relu(x)
+        if self.num_autoencoder_layers == 3:
+            x = self.dropout(x)
+            x = self.layer_3(x)
+            x = self.relu(x)
+        return x
+
+
+class Decoder(nn.Module):
+    def __init__(self,  init_dim, num_autoencoder_layers, dim_autoencoder_layers, dropout):
+        super(Decoder, self).__init__()
+        self.init_dim = init_dim
+        self.num_autoencoder_layers = num_autoencoder_layers
+        self.dim_autoencoder_layers = dim_autoencoder_layers
+        self.layer_3 = None
+        if num_autoencoder_layers == 3:
+            self.layer_3 = nn.Linear(dim_autoencoder_layers[2], dim_autoencoder_layers[1])
+        self.layer_2 = nn.Linear(dim_autoencoder_layers[1], dim_autoencoder_layers[0])
+        self.layer_1 = nn.Linear(dim_autoencoder_layers[0], self.init_dim)
+        self.dropout = nn.Dropout(dropout)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        if self.num_autoencoder_layers == 3:
+            x = self.layer_3(x)
+            x = self.relu(x)
+            x = self.dropout(x)
+        x = self.layer_2(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+        x = self.layer_1(x)
+        return x
+
+
+class SpaceTimeAutoencoder(nn.Module):
+    def __init__(self,  model_config):
+        super().__init__()
+        self.init_dim = model_config['init_dim']
+        self.name_time_model = model_config['name_time_model']
+        self.freeze_layers = model_config.get("freeze_layers", 1.0) > 0.0
+        self.num_autoencoder_layers = model_config['num_autoencoder_layers']
+        dim_autoencoder_layers = model_config['dim_autoencoder_layers'].split(",")
+        self.dim_autoencoder_layers = [int(i) for i in dim_autoencoder_layers]
+        self.dropout = model_config['dropout']
+        self.base_model = None
+
+        if self.name_time_model == "timesformer":
+            self.base_model = TimeSformer()
+        elif self.name_time_model == "r2plus1d_18":
+            self.base_model = R2plus1d_18()
+        elif self.name_time_model == "3d_slowfast":
+            self.base_model = R3D_slowfast()
+        elif self.name_time_model == "r3d":
+            self.base_model = R3D()
+
+        if self.freeze_layers:
+            print("Freeze layers of base model")
+            self.freeze_layers_base_model()
+
+        self.encoder = Encoder(self.init_dim, self.num_autoencoder_layers, self.dim_autoencoder_layers, self.dropout)
+        self.decoder = Decoder(self.init_dim, self.num_autoencoder_layers, self.dim_autoencoder_layers, self.dropout)
+
+    def freeze_layers_base_model(self):
+        for name, param in self.base_model.named_parameters():
+            param.requires_grad = False
+
+    def forward(self, x):
+        embedding = self.base_model(x)
+        latent = self.encoder(embedding)
+        rec_embedding = self.decoder(latent)
+        return embedding, rec_embedding, latent
+
+
+def find_last_checkpoint_file(checkpoint_dir, use_best_checkpoint=False):
+    '''
+    Cerco nella directory checkpoint_dir il file .pth.
+    Se use_best_checkpoint = True prendo il best checkpoint
+    Se use_best_checkpoint = False prendo quello con l'epoca maggiore tra i checkpoint ordinari
+    :param checkpoint_dir:
+    :param use_best_checkpoint:
+    :return:
+    '''
+    print("Cerco il file .pth in checkpoint_dir: {} ".format(checkpoint_dir))
+    list_file_paths = []
+
+    for file in os.listdir(checkpoint_dir):
+        if file.endswith(".pth"):
+            path_file = os.path.join(checkpoint_dir, file)
+            list_file_paths.append(path_file)
+            print("Find: {}".format(path_file))
+
+    print("Number of files .pth: {}".format(int(len(list_file_paths))))
+    path_checkpoint = None
+
+    if len(list_file_paths) > 0:
+
+        if use_best_checkpoint:
+            if os.path.isfile(os.path.join(checkpoint_dir, 'best.pth')):
+                path_checkpoint = os.path.join(checkpoint_dir, 'best.pth')
+        else:
+            if os.path.isfile(os.path.join(checkpoint_dir, 'latest.pth')):
+                path_checkpoint = os.path.join(checkpoint_dir, 'latest.pth')
+
+    return path_checkpoint
